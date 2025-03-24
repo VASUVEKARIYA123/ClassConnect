faaff2f0105ab4f3525cb782aee275b59d214755
VASUVEKARIYA123 <vasuvekariya123@gmail.com>
2025-03-24 09:30:18 +0530

    project add to classroom

--- CLIENT/src/components/TeacherPost/components/NextTask/index.js
+++ CLIENT/src/components/TeacherPost/components/NextTask/index.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import AddLabTaskButton from "../addLabTaskbut"; 
 import AddStudentButton from "../AddStudentButton/addStudentButton";
 import AddFacultyButton from "../AddFacultyButton/addfacultybutton"; 
-import AddProject from "../addProject";
+
 // STYLES
 const Wrapper = styled.div`
   display: flex;
@@ -61,7 +61,7 @@ export default () => {
   <AddLabTaskButton/>
   <AddStudentButton/>
   <AddFacultyButton/>
-  
+
   </Wrapper>
   )
 }
